d947eb09d9a9a62e4a8435c38cc220f9d6010147
Udoka_AM <udoka.eth@gmail.com>
2025-04-22 23:57:44 +0100

    Update lib.rs

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -7,7 +7,7 @@ use anchor_lang::{
 declare_id!("6ptzmLTi6NvwU6LfP3XCKST3uRxxRvDZvMeywLaYdrJ6");
 
 pub mod instructions;
-pub mod state;:
+pub mod state;
 
 
 #[program]
@@ -116,7 +116,7 @@ impl<'info> Payment<'info> {
         transfer(cpi_ctx, amount)
     }
 
-}
+}}
 
 #[derive(Accounts)]
 pub struct Close<'info> {
